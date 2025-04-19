05ce3897ab54a5289eb3ae4121160608c1867c2c
Kenny Levinsen <kl@kl.wtf>
2025-04-20 00:58:33 +0200

    HACK HACK Revert "ipc-server: Force modeset if needed after executing commands"
    This reverts commit a2c73c9b8b426417e0253ea0420b98298af1c963.

--- sway/desktop/output.c
+++ sway/desktop/output.c
@@ -473,10 +473,6 @@ void request_modeset(void) {
 	}
 }
 
-bool modeset_is_pending(void) {
-	return server.delayed_modeset != NULL;
-}
-
 void force_modeset(void) {
 	if (server.delayed_modeset != NULL) {
 		wl_event_source_remove(server.delayed_modeset);
